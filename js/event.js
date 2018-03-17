22865f9907e1fc624e2ecc686bdfde396de0a770
Daniel Lin <lindanny9721@gmail.com>
2018-03-17 20:37:04 +0000

    Working project

--- js/event.js
+++ js/event.js
@@ -1,6 +1,33 @@
-$(document).ready(function() {
-            $("#start").hide();
+var name;
+function newEventGen() {
+                var eventNames = ["maintenance", "supplyrun", "Enemyship","Explosion","Ambush"];
+                var ranNumber = Math.floor(Math.random() * Math.floor(eventNames.length));
+                name = eventNames[ranNumber];
+                // var ranEvent = events[ranNumber].text;
+                // console.log(ranEvent);
+                $("h3").html(events[name].text);
+                $("#choice1").html(events[name].choices[0].text);
+                $("#choice2").html(events[name].choices[1].text);
+                $("#stats").val(Supplies +"  "+Morale +"  " +Militarypower+"  " + Conditions);
+                $("#stats").append()
+}
 
+
+
+function checkLose() {
+               if (Supplies == 0 || Morale == 0 || Conditions == 0 || Militarypower == 0) {
+                    alert("GameOver!");
+                } else {
+                   newEventGen(); 
+                }   
+}
+
+$(document).ready(function() {
+            $("#secondpart").hide();
+            $("#start").hide();
+            $("#scenario").hide();
+            $("#engine").hide;
+            
             $("#name").keydown(function(e) {
                 console.log(e.which);
                 if (e.which === 13) {
@@ -8,39 +35,80 @@ $(document).ready(function() {
                 }
             });
 
-
-
             $("#enter").click(function() {
                 $("#container").hide();
                 $("#start").show();
                 var name = $("#name").val();
 
-
                 $("#namedisplay").html("Welcome " + name + "!");
             });
+            
             $("#start").click(function() {
                 $("#namedisplay").hide();
                 $("#start").hide();
+                $("#secondpart").show();
+                $("#containertwo").hide();
+                newEventGen();
+                $("#scenario").show();
+                $("#engine").show();
             });
-            var Supplies = 100;
-            var Morale = 100;
-            var Conditions = 100;
-            var Strength = 100;
-            var Days = 0;
-            var events = [{
-                maintenance: {
-                    image: "",
-                    text: "Each machine on board the ship requires maintence to make sure everything is running smoothly",
-                    choices: [{
-                            text: "check engine room",
-                            supplies: -10,
-                            morale: -13,
-                            conditions: +25
-                        },
-                        {
-                            text: "Ignore for now.",
-                            conditions: -25,
-                            morale: +13
+
+             $("#choice1").click(function() {
+                Supplies = Supplies + events[name].choices[0].supplies;
+                // console.log(events[name].choices[0].supplies);
+                console.log(Supplies);
+                Morale = Morale + events[name].choices[0].morale;
+                console.log(Morale);
+                Conditions = Conditions + events[name].choices[0].conditions;
+                console.log(Conditions);
+                Militarypower = Militarypower + events[name].choices[0].militarypower;
+                Days ++;
+                checkLose();
+                // $("h2").html([events[0]]);
+                // if (Days >= 0 && Days <= 10) {
+                //     console.log(Object.values(events));
+                // }
+                // else if (Days >= 11 && Days <= 20) {
+                //     console.log(Object.values(events));
+                // }
+                // $("h2").html(events[Math.floor(Math.random() * Math.floor(event.length));]);
+            });
+            $("#choice2").click(function() {
+                Supplies = Supplies + events[name].choices[1].supplies;
+                // console.log(events[name].choices[0].supplies);
+                console.log(Supplies);
+                Morale = Morale + events[name].choices[1].morale;
+                console.log(Morale);
+                Conditions = Conditions + events[name].choices[1].conditions;
+                console.log(Conditions);
+                Militarypower = Militarypower + events[name].choices[1].militarypower;
+                Days ++;
+                checkLose();
+                // $("h2").html(events[Math.floor(Math.random() * Math.floor(event.length));]);        
+            });
+                
+});
+            function getImageUrl(){
+                APILink = "https://pixabay.com/api/?key=8399109-347a1179d345f8bfcade2d937&q=ship+engine&image_type=photo";
+                return $.get(APILink).then(res => res.hits[0].webformatURL);
+            }
+            var events = {
+               maintenance: {
+                     image: "",
+                     text: "Each machine on board the ship requires maintence to make sure everything is running smoothly",
+                     choices: [{
+                             text: "check engine room",
+                             supplies: -10,
+                             morale: -10,
+                             conditions: 25,
+                             militarypower: 0
+                         },
+                         {
+                             text: "Ignore for now.",
+                             conditions: -25,
+                             morale: 13,
+                             militarypower:0,
+                            supplies:0,
                         }
                     ]
                 },
@@ -49,84 +117,87 @@ $(document).ready(function() {
                     text: "Supplies such as food,fire arms, and cleaning tools are needed on the ship",
                     choices: [{
                             text: "Go on a supply run",
-                            supplies: +30,
-                            morale: -15,
-                            conditions: -5
+                            supplies: 40,
+                            militarypower: -15,
+                            conditions: -12,
+                            morale:0,
                         },
                         {
                             text: "Don't go on a supply run",
-                            conditions: -25,
-                            morale: +13
+                            conditions: 13,
+                            morale: 27,
+                            supplies: -10,
+                            militarypower:0,
                         }
                     ]
                 },
-                maintenance: {
+                Enemyship: {
                     image: "",
-                    text: "Each machine on board the ship requires maintence to make sure everything is running smoothly",
+                    text: "A Enemyship is approaching your ship and crew could be in danger ",
                     choices: [{
-                            text: "check engine room",
-                            supplies: -10,
-                            morale: -13,
-                            conditions: +25
+                            text: "Engage enemy ship",
+                            supplies: 30,
+                            morale: 40,
+                            conditions: -30,
+                            militarypower: -35
                         },
                         {
-                            text: "Ignore for now.",
-                            conditions: -25,
-                            morale: +13
+                            text: "Avoide enemyship.",
+                            conditions: 25,
+                            morale: -13,
+                            supplies: -15,
+                            militarypower: 25
                         }
                     ]
                 },
-                maintenance: {
+                Explosion: {
                     image: "",
-                    text: "Each machine on board the ship requires maintence to make sure everything is running smoothly",
+                    text: "there are two explosions on the ship one explosion in the engine room one explosion in the food court you have to chose one to take out first",
                     choices: [{
-                            text: "check engine room",
-                            supplies: -10,
-                            morale: -13,
-                            conditions: +25
+                            text: "Put out fire in the engine room first.",
+                            supplies: -37,
+                            conditions: 40,
+                            militarypower:0,
+                            morale:0,
                         },
                         {
-                            text: "Ignore for now.",
-                            conditions: -25,
-                            morale: +13
+                            text: "put out fire in the Food court first.",
+                            conditions: -38,
+                            supplies: 37,
+                            militarypower:0,
+                            morale:0,
                         }
                     ]
                 },
-                maintenance: {
+                Ambush: {
                     image: "",
-                    text: "Each machine on board the ship requires maintence to make sure everything is running smoothly",
+                    text: "There is an enemy ship  close by docked on a island but they are not aware of your position  do you ambush them or keep moving, they might have supplies",
                     choices: [{
-                            text: "check engine room",
-                            supplies: -10,
-                            morale: -13,
-                            conditions: +25
+                            text: "check out the island and see what you can find",
+                            militarypower:-25,
+                            morale:0,
+                            supplies:0,
+                            conditions:0,
                         },
                         {
-                            text: "Ignore for now.",
-                            conditions: -25,
-                            morale: +13
+                            text: "Keep going stick to the path",
+                            morale:50,
+                            supplies:0,
+                            conditions:0,
+                            militarypower:0,
                         }
                     ]
                 }
-            }];
-            var ranEvent = events[Math.floor(Math.random() * Math.floor(event.length))].text;
-            $("h2").html(ranEvent);
-            $("choice1").click(function() {
-                        Morale ++;
-                        Conditions = Conditions + 20;
-                        Day++;
-                        $("h2").html([events[0]]);
-                        if (Days >= 0 && Days <= 10) {
-                            console.log(Object.values(events));
-                        }
-                        else if (Days >= 11 && Days <= 20) {
-                            console.log(Object.values(events));
-                        }
-                        // $("h2").html(events[Math.floor(Math.random() * Math.floor(event.length));]);
-                    }); $("choice2").click(function() {
-                    Days++;
-                    Morale++;
-                    Conditions - 1;
-                    // $("h2").html(events[Math.floor(Math.random() * Math.floor(event.length));]);        
-                });
-            });
+            };
+            
+            getImageUrl().then(url => (events.maintenance.image = url))
+
+            var Supplies = 100;
+            var Morale = 100;
+            var Conditions = 100;
+            var Militarypower = 100;
+            var Days = 0;
+
+Supplies + " " + Morale
+     
+    
